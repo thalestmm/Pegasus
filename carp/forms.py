d657f845b633f4b345e1b9de92589c07209d816f
thalestmm <meier.thales@gmail.com>
2022-10-24 13:45:38 -0300

    carp form test

---
+++ carp/forms.py
@@ -0,0 +1,38 @@
+from django import forms
+
+
+class CarpForm(forms.Form):
+    name            = forms.CharField(max_length=50, label="Nome do Piloto")
+    trigram         = forms.CharField(max_length=3, label="Trigrama")
+
+    drop_height     = forms.IntegerField(label="ALTURA do lançamento (ft)")
+    terrain_alt     = forms.IntegerField(label="ALTITUDE da ZL (ft)")
+    point_of_impact = forms.IntegerField(label="ALTITUDE do Ponto de Impacto (ft)")
+
+    pressure        = forms.IntegerField(label="Pressão (hPa)")
+    temperature     = forms.FloatField(label="Temperatura (ºC)")
+
+    speed           = forms.IntegerField(label="Velocidade (kt)")
+    mag_course      = forms.IntegerField(label="Eixo de Lançamento (º)")
+
+    # DADOS BALÍSTICOS
+    chute_selection = forms.ChoiceField(choices=(
+        "T-10 AC/RAC",
+        "T-10 C/D, MC1-1C, RALC",
+        "ASA",
+        "G11 A, G12 D",
+        "G13, G14",
+        "STAB"
+    ), initial="T-10 AC/RAC", label="Tipo de Paraquedas")
+    chute_amount      = forms.IntegerField(label="Quantidade de Paraquedas", initial=1)
+    rate_of_fall      = forms.FloatField(label="Rate of Fall (RF)", initial=14.5)
+    vertical_distance = forms.IntegerField(label="Vertical Distance (VD)", initial=180)
+    dec_quocient      = forms.FloatField(label="Deceleration Quocient (DC)", initial=1.6)
+    tfc               = forms.FloatField(label="Time of Fall Constant (TFC)", initial=5.4)
+    exit_time         = forms.FloatField(label="Exit Time (ET)", initial=0.2)
+
+    measure_unit      = forms.ChoiceField(choices=(
+        "Metros (m)",
+        "Jardas (yd)"
+    ), label="Unidade de Medida", initial="Metros (m)")
+
--- carp/views.py
+++ carp/views.py
@@ -1,7 +1,29 @@
 from django.shortcuts import render
+from .forms import CarpForm
 
 # Create your views here.
 
 
 def carp_form(request):
-    return render(request, 'carp/carp_form.html', {})
+
+    if request.method == "POST":
+
+        form = MissionPlannerForm(
+            {
+                "project": Project.objects.get(registry=aircraft_registry),
+                "trip_weight": request.POST['trip_weight'],
+                "takeoff_time": request.POST['takeoff_time']
+            }
+        )
+
+        if form.is_valid():
+            # TODO: ADD FLIGHT PLANNING LOGIC AND SEND REQUEST TO NEXT VIEW
+
+            return render_mission(request, form_data=form.cleaned_data, package=request.POST.items())
+
+    else:
+        form = CarpForm()
+
+    return render(request, 'carp/carp_form.html', {
+        "form": form
+    })
