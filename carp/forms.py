268a5c03d63a376dab153106515a484f07939692
thalestmm <meier.thales@gmail.com>
2022-10-26 20:23:48 -0300

    carp form brackets

--- carp/forms.py
+++ carp/forms.py
@@ -16,14 +16,14 @@ class CarpForm(forms.Form):
     mag_course      = forms.IntegerField(label="Eixo de Lançamento (º)")
 
     # DADOS BALÍSTICOS
-    chute_selection = forms.ChoiceField(choices=(
+    chute_selection = forms.ChoiceField(choices=[
         (1, "T-10 AC/RAC"),
         (2, "T-10 C/D, MC1-1C, RALC"),
         (3, "ASA"),
         (4, "G11 A, G12 D"),
         (5, "G13, G14"),
         (6, "STAB")
-    ), initial=1, label="Tipo de Paraquedas")
+    ], initial=1, label="Tipo de Paraquedas")
     chute_amount      = forms.IntegerField(label="Quantidade de Paraquedas", initial=1)
     rate_of_fall      = forms.FloatField(label="Rate of Fall (RF)", initial=14.5)
     vertical_distance = forms.IntegerField(label="Vertical Distance (VD)", initial=180)
@@ -31,8 +31,8 @@ class CarpForm(forms.Form):
     tfc               = forms.FloatField(label="Time of Fall Constant (TFC)", initial=5.4)
     exit_time         = forms.FloatField(label="Exit Time (ET)", initial=0.2)
 
-    measure_unit      = forms.ChoiceField(choices=(
+    measure_unit      = forms.ChoiceField(choices=[
         ("M", "Metros (m)"),
         ("YD", "Jardas (yd)")
-    ), label="Unidade de Medida", initial="M")
+    ], label="Unidade de Medida", initial="M")
 
